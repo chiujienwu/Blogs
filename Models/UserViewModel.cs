d1c8221f590df28208fbd515d1ccd7d8ebaa2186
jgrissom <jgrissom91@gmail.com>
2019-03-23 16:18:15 -0500

    RoleAdmin/Edit http post
    1) Update User View Model
    2) Create RoleAdmin/Edit controller method to handle http post requests

--- Models/UserViewModel.cs
+++ Models/UserViewModel.cs
@@ -31,4 +31,13 @@ namespace Blogs.Models
         public IEnumerable<AppUser> Members { get; set; }
         public IEnumerable<AppUser> NonMembers { get; set; }
     }
+
+    public class RoleModificationModel
+    {
+        [Required]
+        public string RoleName { get; set; }
+        public string RoleId { get; set; }
+        public string[] IdsToAdd { get; set; }
+        public string[] IdsToDelete { get; set; }
+    }
 }
