c274d436391f1afbebefd3b083136abc08f42fa3
jgrissom <jgrissom91@gmail.com>
2019-03-23 13:34:41 -0500

    RoleAdmin
    1) Add RoleAdmin controller
    2) Add RoleAdmin/Index controller method
    3) Add related view (display list of roles)
    4) Create links to/from Admin/Index and RoleAdmin/Index

---
+++ Controllers/RoleAdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blogs.Controllers
+{
+    public class RoleAdminController : Controller
+    {
+        private RoleManager<IdentityRole> roleManager;
+        public RoleAdminController(RoleManager<IdentityRole> roleMgr)
+        {
+            roleManager = roleMgr;
+        }
+
+        public IActionResult Index() => View(roleManager.Roles);
+
+    }
+}
