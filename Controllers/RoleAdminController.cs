5e340dd4c59892090ef2fb00afafcd5d8af85a43
jgrissom <jgrissom91@gmail.com>
2019-03-23 13:40:56 -0500

    Create Roles
    1) Add RoleAdmin/Create controller method to handle http get requests
    2) Add RoleAdmin/Create view - all that is needed to create the role is a name
    3) Add RoleAdmin/Create controller method to handle http post requests

--- Controllers/RoleAdminController.cs
+++ Controllers/RoleAdminController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blogs.Controllers
@@ -13,5 +15,32 @@ namespace Blogs.Controllers
 
         public IActionResult Index() => View(roleManager.Roles);
 
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Required]string name)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityResult result = await roleManager.CreateAsync(new IdentityRole(name));
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    AddErrorsFromResult(result);
+                }
+            }
+            return View(name);
+        }
+
+        private void AddErrorsFromResult(IdentityResult result)
+        {
+            foreach (IdentityError error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+        }
     }
 }
