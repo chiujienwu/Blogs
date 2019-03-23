752cb3c0a94d81d9aaadd780b2a32bc1b3f9e22e
jgrissom <jgrissom91@gmail.com>
2019-03-23 13:05:40 -0500

    Modify the User View Model
    1) Define the model class to support user login
    2) Utilize the UIHint attribute to ensure that the tag helper will render the appropriate form field (email, password)

--- Models/UserViewModel.cs
+++ Models/UserViewModel.cs
@@ -11,4 +11,15 @@ namespace Blogs.Models
         [Required]
         public string Password { get; set; }
     }
+
+    public class LoginModel
+    {
+        [Required]
+        [UIHint("email")] // ensures the taghelper renders the appropriate form field
+        public string Email { get; set; }
+
+        [Required]
+        [UIHint("password")] // ensures the taghelper renders the appropriate form field
+        public string Password { get; set; }
+    }
 }
