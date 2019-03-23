3b407b74ccd0ac57489726192bb84d8bbcae2353
jgrissom <jgrissom91@gmail.com>
2019-03-23 13:51:01 -0500

    Add RoleAdmin/Delete controller method to respond to http post requests

--- Controllers/RoleAdminController.cs
+++ Controllers/RoleAdminController.cs
@@ -35,6 +35,29 @@ namespace Blogs.Controllers
             return View(name);
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            IdentityRole role = await roleManager.FindByIdAsync(id);
+            if (role != null)
+            {
+                IdentityResult result = await roleManager.DeleteAsync(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    AddErrorsFromResult(result);
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "No role found");
+            }
+            return View("Index", roleManager.Roles);
+        }
+
         private void AddErrorsFromResult(IdentityResult result)
         {
             foreach (IdentityError error in result.Errors)
